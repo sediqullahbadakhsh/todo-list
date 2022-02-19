76c67fd42f0c5e44aea9b2d67da32cba57934a44
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-02-19 04:56:10 +0430

    linter issues and added featurs

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const express = require("express");
-const webpack = require("webpack");
-const webpackDevMiddleware = require("webpack-dev-middleware");
+// const express = require('express');
+// const webpack = require('webpack');
+// const webpackDevMiddleware = require('webpack-dev-middleware');
 
-const app = express();
-const config = require("./webpack.config.js");
-const compiler = webpack(config);
+// const app = express();
+// const config = require('./webpack.config.js');
 
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-app.use(
-  webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-  })
-);
+// const compiler = webpack(config);
 
-// Serve the files on port 3000.
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!\n");
-});
+// // Tell express to use the webpack-dev-middleware and use the webpack.config.js
+// // configuration file as a base.
+// app.use(
+//   webpackDevMiddleware(compiler, {
+//     publicPath: config.output.publicPath,
+//   }),
+// );
+
+// // Serve the files on port 3000.
+// app.listen(3000, () => {
+//   console.log('Example app listening on port 3000!\n');
+// });
